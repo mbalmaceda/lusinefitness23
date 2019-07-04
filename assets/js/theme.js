94d7f408213a9ab6a9b36dca1b13edbefc954cc5
Mirko Balmaceda <mbalmaceda.17@gmail.com>
2019-07-04 00:33:01 -0400

    Propuesta para carousel de partners

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -415,5 +415,26 @@
                 }
             });
         }
+
+        $('.customer-logos').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            dots: false,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2
+                }
+            }]
+        });
     });
 })(jQuery);
