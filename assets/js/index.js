ffb09d555f5229af8e7a0f993dfd864450d37803
vn08q68 <mirko.balmaceda@walmart.com>
2021-04-21 10:05:03 -0400

    Fix cache browser and prices adjustment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    swal.fire({
-        title: '<h1><strong><u>OUVERT</u></strong><h1>',
-        icon: 'info',
-        html:
-          '<h3>Pour les moins de 20 ans et personnel avec Certificats médical</h3>',
-        showCloseButton: true,
-        focusConfirm: true,
-        confirmButtonText:
-          '<i class="fa fa-thumbs-up"></i> OK!',
-        confirmButtonAriaLabel: 'OK!'
-      })
+    // swal.fire({
+    //     title: '<h1><strong><u>OUVERT</u></strong><h1>',
+    //     icon: 'info',
+    //     html:
+    //       '<h3>Pour les moins de 20 ans et personnel avec Certificats médical</h3>',
+    //     showCloseButton: true,
+    //     focusConfirm: true,
+    //     confirmButtonText:
+    //       '<i class="fa fa-thumbs-up"></i> OK!',
+    //     confirmButtonAriaLabel: 'OK!'
+    //   })
 });
